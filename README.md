7c6d8a8b3157e370f76c6443b647c79f104ff39d
Kilian McMahon <kilmc@fastmail.com>
2023-03-30 21:28:15 +0200

    chore: update package in prep for release

---
+++ README.md
@@ -0,0 +1,11 @@
+**Getting started:**
+
+- Generate the new repo from the template [js-packages](https://github.com/kilmc/js-package)
+- Update the `package.json`
+  - name
+  - description
+- Add changeset bot to GitHub repo
+
+# Project
+
+This is a template for a typescript-based JS library
---
+++ rollup.config.mjs
@@ -0,0 +1,19 @@
+import pkg from './package.json' assert { type: 'json' };
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/main.ts',
+  output: [
+    {
+      name: 'libraryBundleName',
+      file: pkg.main,
+      format: 'umd',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+  plugins: [terser(), typescript({ outDir: 'dist' })],
+};
